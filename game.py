95a23038f7de6a071a7662c099d4d5c68b61edd3
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-10 16:07:58 -0600

    drawing objects - alternating strawberries and apples on a grid

--- game.py
+++ game.py
@@ -6,17 +6,6 @@ screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode([screen_height, screen_height])
 
-# Create a new instance of Surface
-surface = pygame.Surface((50, 50))
-surface.fill((255, 111, 33))
-
-# Clear screen
-screen.fill((255, 255, 255))
-
-# Draw the surface
-screen.blit(surface, (100, 120))
-
-
 # Creat the game loop
 running = True 
 while running: 
@@ -25,14 +14,41 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Clear screen
+    screen.fill((255, 255, 255))
 
     #----------------------------------------------------------
     #YOUR DRAWING
     
-    
+    class GameObject(pygame.sprite.Sprite):
+        def __init__(self, x, y, image):
+            super(GameObject, self).__init__()
+            self.surf = pygame.image.load(image)
+            self.x = x
+            self.y = y
+
+        def render(self, screen):
+            screen.blit(self.surf, (self.x, self.y))
+
+  # #draw the fruits
+    for num in range (0,9):
+        x = num % 3
+        y = int(num/3)
+
+        if num % 2:
+            fruit = 'strawberry.png'
+
+        else:
+            fruit = 'apple.png'
+
+        newFruit = GameObject (((x+1)*100), ((y+1)*100), fruit)
+        newFruit.render(screen)
+        print(x, y, fruit)
 
     #----------------------------------------------------------
 
 
 	# Update the display
     pygame.display.flip()
+
+
